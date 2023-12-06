c6927dc3c58acf3dd1836395cd583e1e9e03d3fb
drewdrew <drewdrewthis@gmail.com>
2023-12-06 17:23:58 +0100

    Init: Create Nx Nest app

--- apps/telegram-bot-e2e/jest.config.ts
+++ apps/telegram-bot-e2e/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'test-telegram-bot-e2e',
+  displayName: 'telegram-bot-e2e',
   preset: '../../jest.preset.js',
   globalSetup: '<rootDir>/src/support/global-setup.ts',
   globalTeardown: '<rootDir>/src/support/global-teardown.ts',
@@ -15,5 +15,5 @@ export default {
     ],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/test-telegram-bot-e2e',
+  coverageDirectory: '../../coverage/telegram-bot-e2e',
 };
